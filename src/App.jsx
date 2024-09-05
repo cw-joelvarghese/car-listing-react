107db82021509b9b1a77472cd872e8be4935645b
Joel Varghese <joel.v@aepl.com>
2024-09-05 15:24:17 +0530

    Create basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import "./App.css";
+import styles from "src/App.module.css";
+import Filter from "./components/Filter";
+import Listing from "./components/Listing";
 
 function App() {
     return (
-       <>
-       </>
+        <div className={styles.pageContainer}>
+            <div className={styles.content}>
+                <div className={styles.gridHeading}>
+                    <h2 className={styles.primaryHeading}>
+                        1212 Used cars in india
+                    </h2>
+                </div>
+                <div>
+                    <Filter />
+                </div>
+                <div>
+                    <Listing />
+                </div>
+            </div>
+        </div>
     );
 }
 
